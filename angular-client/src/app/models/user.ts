014ba2cb579dafe32fae5776ceca1d0e08ac98d4
Thanakrit Lee <thanakrit.lee.2014@gmail.com>
2018-04-07 16:46:40 +1000

    Add working user service.

--- angular-client/src/app/models/user.ts
+++ angular-client/src/app/models/user.ts
@@ -1,3 +1,8 @@
 export class User {
-    
+    id: number;
+    fname: string;
+    lname: string;
+    contact: string;
+    email: string;
+    address: string;
 }
--- angular-client/src/app/users/users.component.ts
+++ angular-client/src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
+
+  private user_list: User[] = [];
 
   ngOnInit() {
+    console.log('init before');
+    this.getUsers();
+    console.log('init after');
   }
+
+  public getUsers() {
+    console.log('getUser() before');
+    this.userService.getAllUsers().subscribe(
+      response => {
+      this.user_list = response
+      console.log(this.user_list)
+      },
+    );
+    console.log('getUser() after');
+    
+  }
+
+  
 
 }
