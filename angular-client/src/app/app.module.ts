e7f8a0e267cd2856ff15b5ccefbff7401d07a07d
Thanakrit Lee <thanakrit.lee.2014@gmail.com>
2018-04-07 14:06:09 +1000

    Add nav component links.

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HttpModule } from '@angular/http';
+import { UserService } from './services/user.service';
+import { CalendarService } from './services/calendar.service';
+
+import { AppRoutingModule } from './routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -9,7 +15,6 @@ import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { MyTimetableComponent } from './my-timetable/my-timetable.component';
 import { AboutComponent } from './about/about.component';
-
 
 @NgModule({
   declarations: [
@@ -22,7 +27,9 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    AppRoutingModule,
+    HttpModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
